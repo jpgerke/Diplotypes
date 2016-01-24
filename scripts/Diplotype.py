700beaa10535fa565663a005be51316e8617c794
Justin Gerke <justin.gerke@gmail.com>
2016-01-23 20:57:17 -0600

    Added selfing and added gamete probabilities

--- scripts/Diplotype.py
+++ scripts/Diplotype.py
@@ -2,11 +2,17 @@
 """
 Created on Thu Jan 21 19:34:14 2016
 
-@author: justi
+@author: justin gerke
+
+Written in python 3.5, but designed to also work in python 2.7
 """
 from __future__ import print_function
 from collections import abc
 import itertools as it
+import sympy as sym
+from sympy import Symbol
+from collections import defaultdict
+from pprint import pprint
 
 class Diplotype(object):
     """Two locus diplotype"""
@@ -41,10 +47,7 @@ class Diplotype(object):
     
     def __getitem__(self, x):
         return self.origin[x]
-
-    def __len__(self):
-        return len(self.origin)
-    
+ 
     def __contains__(self, x):
         return x in self.alleles
     
@@ -70,25 +73,36 @@ class Diplotype(object):
         Probabilities are:
         (1-r, 1-r, r, r)
         
-        """
-        
-        #if only one origin is present it's easy
-        if len(self.alleles)==1:
-            return([self.maternal[0]] * 4)
-        else:
-            prods = [self.maternal,
-                     self.paternal,
-                     ''.join([self.maternal[0], self.paternal[1]]),
-                     ''.join([self.maternal[1], self.paternal[0]])]
-            return(prods)
+        """    
+        r = Symbol("r")    
+        probs = ((1-r)/2, (1-r)/2, r/2, r/2)
+        prods = (self.maternal,
+                 self.paternal,
+                 ''.join([self.maternal[0], self.paternal[1]]),
+                 ''.join([self.maternal[1], self.paternal[0]]))
+        return tuple(zip(prods, probs))
             
     def selfmate(self):
+        """Return all possible combinations of gametes"""
         gams = self.gametes()
-        return [[gams[x], gams[y]]for x,y in it.product(range(0,4),
-                                                        repeat = 2)]
-                
+        return [( Diplotype((g[0],h[0])), g[1]*h[1] )
+                           for g,h in it.product(gams, repeat = 2)]
+                                                        
+     
 if __name__ == '__main__':        
     b = Diplotype(["AA", "BB"])
     c = Diplotype(["AA", "AA"])
     d = Diplotype(["AB", "CD"])
-    d.selfmate()
+    x = d.selfmate()
+    print("selfing products of AA|BB:")
+    print('\n'.join([str(x) for x in x]))
+    print('\n')
+    probs = defaultdict(float)
+    for y in b.selfmate():
+        probs[str(y[0])] += y[1]
+    print("grouped by genotype:")
+    pprint(probs.items())
+    print('\n')
+    print("AA|AA probability factored:")
+    print(sym.factor(probs['AA|AA']))
+    
